d556e65ca27ce33f608eca9a0654dff00f53a01a
feliun <felipe.polo@gmail.com>
2019-01-29 16:01:01 +0100

    Fixing tabs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = () => {
     client.context.tags[id] = value;
   };
 
-	const start = async ({ config }) => {
+  const start = async ({ config }) => {
     const { key, internalLogging, samplingPercentage, autoCollect: { requests, performance, exceptions, dependencies, console }, context: { tags } } = config;
     if (!key) throw new Error('No insights key has been provided!');
     const { defaultClient } = appInsights
@@ -26,7 +26,7 @@ module.exports = () => {
     if (samplingPercentage) setupSampling(defaultClient)(samplingPercentage);
     tags.forEach(setupTag(defaultClient));
     return defaultClient;
-	};
+};
 
 	return { start };
 };
