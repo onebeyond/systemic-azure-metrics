3dd3406961f55a1f3748610313b8d0eacbc86da4
feliun <felipe.polo@gmail.com>
2019-01-30 11:09:14 +0100

    Allowing broader configuration

--- index.js
+++ index.js
@@ -2,31 +2,50 @@ const appInsights = require('applicationinsights');
 
 module.exports = () => {
 
-  const setupSampling = (client) => (value) => {
-    client.config.samplingPercentage = value;
+  // samplingPercentage, disableAppInsights, etc
+  const setupConfig = (client) => (property, value) => {
+    client.config.property = value;
   };
 
-  const setupTag = (client) => ({ id, value }) => {
-    client.context.tags[id] = value;
+  const setupTag = (client) => (property, value) => {
+    client.context.tags[property] = value;
   };
 
   const start = async ({ config }) => {
-    const { key, internalLogging, samplingPercentage, autoCollect: { requests, performance, exceptions, dependencies, console }, context: { tags } } = config;
+    const {
+      key,
+      internalLogging = true,
+      insightsConfig = {},
+      context: {
+        tags = {},
+      },
+      autoCollect: {
+        requests = true,
+        performance = true,
+        exceptions = true,
+        dependencies = true,
+        console = true
+      },
+    } = config;
     if (!key) throw new Error('No insights key has been provided!');
     appInsights
       .setup(key)
-      .setInternalLogging(!!internalLogging)
-      .setAutoCollectRequests(!!requests)
-      .setAutoCollectPerformance(!!performance)
-      .setAutoCollectExceptions(!!exceptions)
-      .setAutoCollectDependencies(!!dependencies)
-      .setAutoCollectConsole(!!console, !!console)
+      .setInternalLogging(internalLogging)
+      .setAutoCollectRequests(requests)
+      .setAutoCollectPerformance(performance)
+      .setAutoCollectExceptions(exceptions)
+      .setAutoCollectDependencies(dependencies)
+      .setAutoCollectConsole(console, console)
       .start();
 
     const { defaultClient } = appInsights;
 
-    if (samplingPercentage) setupSampling(defaultClient)(samplingPercentage);
-    tags.forEach(setupTag(defaultClient));
+    const configureClient = setupConfig(defaultClient);
+    Object.keys(insightsConfig).forEach((key) => configureClient(key, insightsConfig[key]));
+
+    const configureTag = setupTag(defaultClient);
+    Object.keys(tags).forEach((key) => configureTag(key, tags[key]));
+
     return defaultClient;
   };
 
